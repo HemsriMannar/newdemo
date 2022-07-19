aee32f14d1821f6ee08a1a00bfa937d50efa0931
hemsri <hemsri2001@gmail.com>
2022-07-19 22:05:47 +0530

    changes

--- programming/src/oops/calcarray.java
+++ programming/src/oops/calcarray.java
@@ -1,37 +1,29 @@
-package programbasic;
+package oops;
 
 import java.util.Scanner;
 class calculation{
 	private int a;
 	private int b;
-//	public int getA() {
-//		return a;
-//	}
 	public void setA(int a) {
 		this.a = a;
 	}
-//	public int getB() {
-//		return b;
-//	}
 	public void setB(int b) {
 		this.b = b;
 	}
 	public void calc() {
 		int operator;
-		System.out.println("Enter any choice 1. +,2. -,3. *,4. %");	
+		System.out.println("Enter any choice 1.+ ,2.- ,3.* ,4.% ");	
 		Scanner input = new Scanner(System.in);
 		operator = input.nextInt();
 		switch(operator) {
-		case 1: System.out.println("the answer is:"+(a+b));break;
-		case 2: System.out.println("the answer is:"+(a-b));break;
-		case 3: System.out.println("the answer is"+ a*b);break;
-		case 4: System.out.println("the answer is:"+a/b);break;
-		}
-		
+		case 1: System.out.println("the answer is: "+(a+b));break;
+		case 2: System.out.println("the answer is: "+(a-b));break;
+		case 3: System.out.println("the answer is: "+ a*b);break;
+		case 4: System.out.println("the answer is: "+a/b);break;
+		}		
 	}
 }
 public class calcarray {
-	//private static Scanner sc;
 	public static void main(String[] args) {
 		int a1;
 		int b1;
@@ -39,7 +31,7 @@ public class calcarray {
 		System.out.println("Enter first number");
 		a1=input.nextInt();
 		System.out.println("Enter second number");
-		b1=input.nextInt();	
+		b1=input.nextInt();		
 		calculation c = new calculation();
 		c.setA(a1);
 		c.setB(b1);
